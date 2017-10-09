6bd380d21c9afa22b6f3eb373aab2954d77ff10e
franksun319 <frank.sun.319@gmail.com>
2017-10-09 15:53:20 +0800

    使爬虫更稳健

--- post_request.py
+++ post_request.py
@@ -1,14 +1,30 @@
 # coding=utf-8
 # __author__='Frank Sun'
-# description:
+# description: 指定年份、板块、公告类型，生成公告文件列表
 
 import csv
 import math
+import os
+import random
+import time
 
 import requests
 
-OUTPUT_CSV_FILE = 'D:/My Works/上市企业年报/2016创业板首次上市信息.csv'
-TIME_STAMP = ''
+"""
+#################
+可改配置，酌情更改
+#################
+"""
+START_DATE = '2004-05-28'  # 首家中小板上市企业的日期
+END_DATE = str(time.strftime('%Y-%m-%d'))  # 当前日期，可设置为制定值
+OUTPUT_CSV_FILE = 'D:/My Works/上市企业年报/2016中小板年报/2016中小板板首次上市信息' + \
+                  '_' + START_DATE.replace('-', '') + '-' + END_DATE.replace('-', '') + '.csv'
+# 板块类型：shmb（沪市主板）、szmb（深市主板）、szzx（中小板）、szcy（创业板）
+PLATE = 'szzx' + ';'
+# 公告类型：category_scgkfx_szsh（首次公开发行及上市）、category_ndbg_szsh（年度报告）、category_bndbg_szsh（半年度报告）
+CATEGORY = 'category_scgkfx_szsh' + ';'
+
+##### 固定配置，勿改 #####
 URL = 'http://www.cninfo.com.cn/cninfo-new/announcement/query'
 HEADER = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
@@ -16,36 +32,19 @@ HEADER = {
 }
 MAX_PAGESIZE = 50
 RELOAD_TIMES = 5
+SLEEP_COUNT = random.randint(40, 50)
 
 
 # 参数：页id（没页条目个数由MAX_PAGESIZE控制），是否返回总条目数（bool)
 def get_response(page_num, return_total_count=False):
-    # 中小板首次公开发行及上市信息查询
-    # query = {
-    #     'stock': '',
-    #     'searchkey': '',
-    #     'plate': 'szzx;',
-    #     'category': 'category_scgkfx_szsh;',
-    #     'trade': '',
-    #     'column': 'szse_sme',
-    #     'columnTitle': '历史公告查询',
-    #     'pageNum': page_num,
-    #     'pageSize': MAX_PAGESIZE,
-    #     'tabName': 'fulltext',
-    #     'sortName': '',
-    #     'sortType': '',
-    #     'limit': '',
-    #     'showTitle': 'category_scgkfx_szsh/category/首次公开发行及上市;szzx/plate/中小板',
-    #     'seDate': '请选择日期'
-    # }
-    # 创业版本首次公开发行及上市信息查询
+    time_stamp = START_DATE + '~' + END_DATE
     query = {
         'stock': '',
         'searchkey': '',
-        'plate': 'szcy;',
-        'category': 'category_scgkfx_szsh;',
+        'plate': PLATE,
+        'category': CATEGORY,
         'trade': '',
-        'column': 'szse_gem',
+        'column': 'szse_sme',
         'columnTitle': '历史公告查询',
         'pageNum': page_num,
         'pageSize': MAX_PAGESIZE,
@@ -53,8 +52,8 @@ def get_response(page_num, return_total_count=False):
         'sortName': '',
         'sortType': '',
         'limit': '',
-        'showTitle': 'szcy/plate/创业板;category_scgkfx_szsh/category/首次公开发行及上市',
-        'seDate': '请选择日期'
+        'showTitle': '',
+        'seDate': time_stamp,
     }
     result_list = []
     reloading = 0
@@ -115,12 +114,18 @@ def filter_illegal_filename(filename):
 
 
 if __name__ == '__main__':
+    if os.path.exists(OUTPUT_CSV_FILE):
+        os.remove(OUTPUT_CSV_FILE)
     item_count = get_response(1, True)
     begin_pg = 1
     end_pg = int(math.ceil(item_count / MAX_PAGESIZE))
     with open(OUTPUT_CSV_FILE, 'a', newline='', encoding='gb18030') as csv_out:
         writer = csv.writer(csv_out)
         for i in range(begin_pg, end_pg):
+            if i % SLEEP_COUNT == 0:
+                s = random.randint(8, 15)
+                print('Sleeping ' + str(s) + ' seconds ...')
+                time.sleep(s)
             writer.writerows(get_response(i))
             print('Page ' + str(i) + '/' + str(end_pg) + ' fetched, it contains items: (' +
                   str(1 + (i - 1) * MAX_PAGESIZE) + '-' + str(i * MAX_PAGESIZE) + ')/' + str(item_count) + '.'
